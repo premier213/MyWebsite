3dd3d241aa1b7a1e017c975ea67d3cc0fb8d878c
premier213 <premierp1213@gmail.com>
2021-03-02 16:30:57 +0330

    added router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import styled from "styled-components";
+import Header from "./components/header";
+
+const Body = styled.div`
+  font-family: "Poppins", Arial, Helvetica, sans-serif;
+`;
+
 function App() {
-  return <div className="App">hello world...</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Body className="bg-gray-300">
+          <Header>
+            <Switch>
+              <Route exact path="/">
+                home
+              </Route>
+              <Route path="/about">about</Route>
+              <Route path="/portfolio">portfolio</Route>
+              <Route path="/contact">contact</Route>
+              <Route path="*">404</Route>
+            </Switch>
+          </Header>
+        </Body>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,6 @@
+const Header = (props) => {
+  const { children } = props;
+  return <div>{children}</div>;
+};
+
+export default Header;
