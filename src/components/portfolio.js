accefaf1284cb8d3f6cb73c38c148d63db1d74ed
premier213 <premierp1213@gmail.com>
2021-03-16 17:29:45 +0330

    portfolio edit

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,9 +1,11 @@
+import map from "lodash/map";
 import styled from "styled-components";
 import NavDesktop from "./nav/navDesktop";
 import myWorks from "../data";
 
+const portfoy = map(myWorks, "title").slice(1, 2);
 const Img = styled.div`
-  background-image: url(${myWorks[5].img});
+  background-image: url(${myWorks[0].img});
   background-position: top;
   background-size: 100vw;
 `;
@@ -13,6 +15,7 @@ const Portfolio = () => (
       <Img className="w-100v 2xl:w-50v h-30v bg-yellow rounded-br-25 sm:w-30v sm:h-80v sm:rounded-br-full xl:w-40v xl:h-75v xl:rounded-r-4xl" />
       <div className=" 2xl:my-10h xl:my-10h xl:mx-20 h-60v sm:my-20h sm:w-70v sm:h-80v my-10h mx-12">
         <NavDesktop />
+        {portfoy}
       </div>
     </div>
   </div>
