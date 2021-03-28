019dbb551ea11b81d4df320463968ed0b9f4087d
premier213 <premierp1213@gmail.com>
2021-03-28 17:43:57 +0430

    create contact blank page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,22 @@
 const Contact = () => (
-  <div className="sm:bg-white lg:bg-gray-300 h-screen">contact</div>
+  <div className="sm:bg-white xl:items-center xl:justify-center xl:bg-gray-300 flex min-h-screen">
+    <div className="rounded-4xl sm:flex-row xl:flex-row-reverse xl:w-75v xl:h-75v xl:bg-white flex flex-col">
+      <div className=" 2xl:my-10h xl:my-10h xl:mx-20 h-60v sm:my-20h sm:w-70v sm:h-80v xl:w-40v mx-12">
+        <div className="font-display text-md md:text-3xl xl:mt-12 text-gray-800">
+          2
+        </div>
+        <div className="md:text-xl md:mt-4 md:w-50v xl:w-40v 2xl:w-30v xl:text-lg text-sm">
+          3
+        </div>
+        <div className="font-display text-md md:text-2xl md:mt-8 xl:mt-4 xl:text-md text-gray-800">
+          4
+        </div>
+        <div className=" md:w-50v md:text-xl md:mt-4 xl:w-40v 2xl:w-30v xl:text-lg text-sm">
+          5
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default Contact;
