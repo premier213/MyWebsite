8191700a3d68f65c4890364559cb54f1d8534d81
premier213 <premierp1213@gmail.com>
2021-03-30 23:49:01 +0430

    added social icons animation

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import { motion } from "framer-motion";
 import {
   RiSendPlaneLine,
   RiMailSendLine,
@@ -17,10 +18,10 @@ const Contact = () => {
   }
   return (
     <div className="sm:bg-white xl:items-center xl:justify-center xl:bg-gray-300 flex min-h-screen">
-      <div className="h-80h bg-yellow xl:bg-white w-90v xl:rounded-4xl xl:flex-row-reverse xl:w-75v xl:h-75v">
-        <div className="2xl:my-10h xl:my-10h xl:mx-20 h-60v sm:my-20h sm:w-70v sm:h-80v xl:w-40v mx-12">
+      <div className="h-80h rounded-br-7xl bg-yellow xl:bg-white w-90v xl:rounded-4xl xl:flex-row-reverse xl:w-75v xl:h-75v">
+        <div className="2xl:my-10h xl:my-10h xl:mx-20 h-60v sm:my-20h sm:w-70v sm:h-80v xl:w-40v">
           <NavDesktop />
-          <div className="font-display sm:text-4xl md:text-6xl mx-8 mt-8 text-xl text-white">
+          <div className="font-display sm:text-4xl md:text-6xl mx-8 mt-4 text-xl text-white">
             {getInTouch}
           </div>
           <div className="mx-8 mt-4">
@@ -63,41 +64,61 @@ const Contact = () => {
                 </div>
               </div>
               <div>
-                <button
+                <motion.button
                   className="bg-blue rounded-xl focus:outline-none flex px-4 py-2 mt-4 font-serif text-lg text-white"
                   type="submit"
                 >
                   {send}
                   <RiSendPlaneLine className="mt-1 ml-2 text-2xl" />
-                </button>
+                </motion.button>
               </div>
               <div className="sm:text-4xl flex mt-8 text-2xl text-white">
-                <button className="mx-2" type="button">
+                <motion.button
+                  className="mx-2"
+                  type="button"
+                  whileHover={{ scale: 1.5 }}
+                  whileTap={{ scale: 1.5 }}
+                >
                   <a
                     href="mailto:premier213@outlook.com"
                     title="Email:premier213@outlook.com"
                   >
                     <RiMailSendLine />
                   </a>
-                </button>
-                <button className="mx-2" type="button">
+                </motion.button>
+                <motion.button
+                  className="mx-2"
+                  type="button"
+                  whileHover={{ scale: 1.5 }}
+                  whileTap={{ scale: 1.5 }}
+                >
                   <a href="https://dribbble.com/premier213" title="Dribbble">
                     <RiDribbbleFill />
                   </a>
-                </button>
-                <button className="mx-2" type="button">
+                </motion.button>
+                <motion.button
+                  className="mx-2"
+                  type="button"
+                  whileHover={{ scale: 1.5 }}
+                  whileTap={{ scale: 1.5 }}
+                >
                   <a href="https://github.com/premier213" title="Github">
                     <RiGithubLine />
                   </a>
-                </button>
-                <button className="mx-2" type="button">
+                </motion.button>
+                <motion.button
+                  className="mx-2"
+                  type="button"
+                  whileHover={{ scale: 1.5 }}
+                  whileTap={{ scale: 1.5 }}
+                >
                   <a
                     href="https://www.linkedin.com/in/premier213"
                     title="LinkedIn"
                   >
                     <RiLinkedinLine />
                   </a>
-                </button>
+                </motion.button>
               </div>
             </form>
           </div>
